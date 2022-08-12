bdd6f0d27429ea451ef86f5c7dc7cbfd3e88c59b
성은기 <tech@schaffengott.com>
2022-08-12 15:15:28 +0900

    초기 커밋.

---
+++ testSearchAddress/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace testSearchAddress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gMapControl1 = new GMap.NET.WindowsForms.GMapControl();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // gMapControl1
+            // 
+            this.gMapControl1.Bearing = 0F;
+            this.gMapControl1.CanDragMap = true;
+            this.gMapControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gMapControl1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl1.GrayScaleMode = false;
+            this.gMapControl1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl1.LevelsKeepInMemory = 5;
+            this.gMapControl1.Location = new System.Drawing.Point(0, 0);
+            this.gMapControl1.MarkersEnabled = true;
+            this.gMapControl1.MaxZoom = 2;
+            this.gMapControl1.MinZoom = 2;
+            this.gMapControl1.MouseWheelZoomEnabled = true;
+            this.gMapControl1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl1.Name = "gMapControl1";
+            this.gMapControl1.NegativeMode = false;
+            this.gMapControl1.PolygonsEnabled = true;
+            this.gMapControl1.RetryLoadTile = 0;
+            this.gMapControl1.RoutesEnabled = true;
+            this.gMapControl1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl1.ShowTileGridLines = false;
+            this.gMapControl1.Size = new System.Drawing.Size(800, 450);
+            this.gMapControl1.TabIndex = 5;
+            this.gMapControl1.Zoom = 0D;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(248, 414);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 21);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox1_KeyDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(487, 409);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.gMapControl1);
+            this.Name = "Form1";
+            this.Text = "Move keyword";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMapControl1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ testSearchAddress/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using System.Collections.Generic;
+
+namespace testSearchAddress
+{
+    public partial class Form1 : Form
+    {
+        public static string API_KEY = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                System.Net.IPHostEntry e1 =
+                     System.Net.Dns.GetHostEntry("www.google.com");
+            }
+            catch
+            {
+                gMapControl1.Manager.Mode = AccessMode.CacheOnly;
+                MessageBox.Show("No internet connection avaible, going to CacheOnly mode.",
+                      "GMap.NET - Demo.WindowsForms", MessageBoxButtons.OK,
+                      MessageBoxIcon.Warning);
+            }
+
+            // center red cross 제거
+            gMapControl1.ShowCenter = false;
+
+            GMaps.Instance.Mode = AccessMode.ServerAndCache;
+            gMapControl1.CacheLocation = Application.StartupPath + "data.gmdp";
+            GMapProviders.GoogleMap.ApiKey = API_KEY;
+            gMapControl1.MapProvider = GMapProviders.GoogleMap;
+            gMapControl1.DragButton = MouseButtons.Left;
+
+            gMapControl1.MaxZoom = 20;
+            gMapControl1.MinZoom = 2;
+            gMapControl1.Zoom = 16;
+
+        }
+
+        private void TextBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                Button1_Click(sender, e);
+            }
+        }
+
+        // Search
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string keyword = textBox1.Text;
+            if (keyword.Length > 0)
+            {
+                GeoCoderStatusCode result = gMapControl1.GetPositionByKeywords(keyword, out PointLatLng point);
+
+                if (result == GeoCoderStatusCode.OK)
+                {
+                    gMapControl1.Position = point;
+                }
+            }
+        }
+
+        // point -> address 표시
+        private void GetAddress(PointLatLng point)
+        {
+            GeoCoderStatusCode result = GMapProviders.GoogleMap.GetPlacemarks(point, out List<Placemark> place);
+            if (result == GeoCoderStatusCode.OK && place != null)
+            {
+                foreach (var pl in place)
+                {
+                    if (!string.IsNullOrEmpty(pl.PostalCodeNumber))
+                    {
+                        Console.WriteLine("Accuracy: " + pl.Accuracy + ", " + pl.Address + ", PostalCodeNumber: " + pl.PostalCodeNumber);
+                    }
+                }
+            }
+        }
+    }
+}
